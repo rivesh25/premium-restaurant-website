1dd78014b91b5b4a55bc70f4267f0979132e9a5f
rivesh25 <riveshkumar2004@gmail.com>
2026-04-06 20:35:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { ScrollTrigger, SplitText } from "gsap/all"
+import gsap from "gsap"
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <div>Hello, World!</div>
+  )
+}
+
+export default App

cb0ba26eb0ef84c9d77ac3d4b9ba1b8104758757
rivesh25 <riveshkumar2004@gmail.com>
2026-04-07 16:07:20 +0530

    Hero section scroll animation implementation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({ maxWidth: 767 });
 
     useGSAP(() => {
         const heroSplit = new SplitText('.title', { type: 'chars, words' });
@@ -37,26 +41,53 @@ const Hero = () => {
             .to('.left-leaf', { y: 200 }, 0)
             .to('.right-leaf', { y: 200 }, 0);
 
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        })
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            })
+        }
+
     }, [])
 
     return (
-        <section className="noisy" id="hero">
-            <h1 className="title">MOJITO</h1>
-            <img src="/images/hero-left-leaf.png" alt="left-leaf" className="left-leaf" />
-            <img src="/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf" />
-            <div className="body">
-                <div className="content">
-                    <div className="space-y-5 hidden md:block">
-                        <p>Cool. Crisp. Classic.</p>
-                        <p>Sip the spirit <br />of Summer</p>
-                    </div>
-                    <div className="view-cocktails">
-                        <p className="subtitle">Every cocktail on our menu is a blend of premium ingredients, creative flair and timeless recipes designed to delight your senses.</p>
-                        <a href="#cocktails">View Cocktails</a>
+        <div>
+            <section className="noisy" id="hero">
+                <h1 className="title">MOJITO</h1>
+                <img src="/images/hero-left-leaf.png" alt="left-leaf" className="left-leaf" />
+                <img src="/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf" />
+                <div className="body">
+                    <div className="content">
+                        <div className="space-y-5 hidden md:block">
+                            <p>Cool. Crisp. Classic.</p>
+                            <p>Sip the spirit <br />of Summer</p>
+                        </div>
+                        <div className="view-cocktails">
+                            <p className="subtitle">Every cocktail on our menu is a blend of premium ingredients, creative flair and timeless recipes designed to delight your senses.</p>
+                            <a href="#cocktails">View Cocktails</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            <video
+                ref={videoRef}
+                src="/videos/output.mp4"
+                muted
+                playsInline
+                preload="auto"
+            />
+        </div>
     )
 }
 
